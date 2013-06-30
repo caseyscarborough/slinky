b7108a06f90f808670f39ab3c0408ee18b354c6a
Casey Scarborough <caseyscarborough@gmail.com>
2013-06-29 21:40:16 -0400

    Added basic styling.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'jquery-rails'
+gem 'bootstrap-sass', '2.1'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Slinky::Application.routes.draw do
   match '/' => "home#index"
+  match '/login' => "session#login"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
