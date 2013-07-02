b0785eeb327ae554a49d4b72f8de6322200f5dd6
Casey Scarborough <caseyscarborough@gmail.com>
2013-07-02 12:29:28 -0400

    Fixed formatting issue on user page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,33 @@ $(document).ready(function() {
     $(".message").delay(4000).fadeOut(500);
     $(".alert").hide().fadeIn(500);
     $(".alert").delay(4000).fadeOut(500);
+
+    $('#new_user_form').validate({
+        rules: {
+            "user[first_name]": {
+                required: true
+            },
+            "user[last_name]": {
+                required: true
+            },
+            "user[password]": {
+                required: true
+            },
+            "user[email]": {
+                required: true,
+                maxlength: 100
+            },
+            "user[password_confirmation]": {
+                required: true,
+                equalTo: "#user_password",
+                minlength: 6
+            }
+        },
+        messages: {
+            "user[password_confirmation]": {
+                equalTo: "Passwords must match.",
+                minlength: "Password must be at least 6 characters."
+            }
+        }
+    });
 })
